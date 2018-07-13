4e847aa58ee3b6f68220bd6e5d5e56eda554c722
marissa-a <marissa.andrade@verizon.net>
2018-07-13 16:33:37 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,26 +1,25 @@
 # person.rb
 class Person
   def initalize(name,job)
-    @name=name
-    @job=job
+    @name = name
+    @job = job
   end
   
   def name
-    @name="Beyonce"
+    @name = "Beyonce"
   end
   
-  def name=(new_name)
-    @name=new_name
+  def name = (new_name)
+    @name = new_name
   end
   
   def job
-    @job="Singer"
+    @job = "Singer"
   end
   
-  def job=(new_job)
-    @job=new_job
+  def job = (new_job)
+    @job = new_job
   end
-  
 end
 
     
