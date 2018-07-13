1dd75db930899071232af6a89aca951af2faddde
marissa-a <marissa.andrade@verizon.net>
2018-07-13 16:33:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,23 @@
 # dog.rb
 class Dog
 def initalize(name,breed)
-  @name=name
-  @breed=breed
+  @name = name
+  @breed = breed
 end
 
 def name
-  @name="Fido"
+  @name = "Fido"
 end
 
-def name=(new_name)
-  @name=new_name
+def name = (new_name)
+  @name = new_name
 end
 
 def breed
-  @breed="Beagle"
+  @breed ="Beagle"
+end
 
-def breed=(new_breed)
-  @breed=new_breed
+def breed = (new_breed)
+  @breed = new_breed
  end
 end
